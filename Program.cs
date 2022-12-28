8e47b87a4864f15b09fd4d15c7cfcd238ea8e8db
Moazz <moazzemh222@gmail.com>
2022-12-28 23:34:38 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> items = new Stack<string>();
+            items.Push("Laptop-001");
+            items.Push("Laptop-002");
+            items.Push("Laptop-003");
+
+            Console.WriteLine(items.Pop());
+            Console.WriteLine(items.Pop());
+            Console.WriteLine(items.Pop());
+        }
+    }
+}
